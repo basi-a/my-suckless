6d1fab8a254aa29a8aa4c72e635421129e5b2e1c
basi-a <basi-a@outlook.com>
2022-12-27 20:17:08 +0800

    update script

--- suckless-install.sh
+++ suckless-install.sh
@@ -19,10 +19,18 @@ slstatus(){
     sudo make && sudo make clean install
     cd ../
 }
+autostart_and_xinitrc(){
+    mkdir $HOME/.dwm
+    cp dwm/script/autostart.sh $HOME/.dwm/autostart.sh
+    touch $HOME/.xinitrc
+    echo "export LANG="zh_CN.UTF-8"" >> $HOME/.xinitrc
+    echo "exec dwm" >> $HOME/.xinitrc
+}
 main(){
     dwm
     st
     slock
     slstatus
+    autostart_and_xinitrc
 }
 main
