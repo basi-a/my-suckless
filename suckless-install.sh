a716f221c98294bb2aad4c5011fe3a9a1847477d
basi-a <basi-a@outlook.com>
2022-12-27 20:08:31 +0800

    update script

--- suckless-install.sh
+++ suckless-install.sh
@@ -2,18 +2,22 @@
 dwm(){
     cd dwm
     sudo make && sudo make clean install
+    cd ../
 }
 st(){
     cd st
     sudo make && sudo make clean install
+    cd ../
 }
 slock(){
     cd slock
     sudo make && sudo make clean install
+    cd ../
 }
 slstatus(){
     cd slstatus
     sudo make && sudo make clean install
+    cd ../
 }
 main(){
     dwm
