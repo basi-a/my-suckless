3b5b396f0689615abd3d7dff6b6dc0c9ca4f473c
basi-a <basi-a@outlook.com>
2022-12-28 17:31:52 +0800

    update script

--- reinstall.sh
+++ reinstall.sh
@@ -20,30 +20,33 @@ reinstall_slock(){
     sudo make && sudo make clean install && cd ../
 }
 main(){
-    select=$1
     case $select in
-        dwm)
-            echo "reinstall dwm......"
-            reinstall_dwm
-            ;;
-        st)
-            echo "reinstall st......"
-            reinstall_st
-            ;;
-        slstatus)
-            echo "reinstall slstatus......"
-            reinstall_slstatus
-            ;;
-        slock)
-            echo "reinstall slock"
-            reinstall_slock
-            ;;
-        *)
-            echo "reinstall all......"
-            reinstall_dwm
-            reinstall_st
-            reinstall_slock
-            reinstall_slstatus
+    dwm)
+        echo "reinstall dwm......"
+        reinstall_dwm
+        ;;
+    st)
+        echo "reinstall st......"
+        reinstall_st
+        ;;
+    slstatus)
+        echo "reinstall slstatus......"
+        reinstall_slstatus
+        ;;
+    slock)
+        echo "reinstall slock"
+        reinstall_slock
+        ;;
+    all)
+        echo "reinstall all......"
+        reinstall_dwm
+        reinstall_st
+        reinstall_slock
+        reinstall_slstatus
+        ;;
+    *)
+        echo "Error parameter"
     esac
 }
+select=$1
 main
