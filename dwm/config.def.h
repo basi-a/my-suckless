265d8af4ab22b786dbec3941d8f5610958cdb17c
basi-a <basi-a@outlook.com>
2022-12-31 13:02:06 +0800

    update rules

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -37,7 +37,6 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "chromium", NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "wemeet",	  NULL,	      NULL,		  0,		    1,	         -1 },
-	{ "linuxqq",  NULL,	      NULL,		  0,		    1,	         -1 },
 	{ "dbeaver",  NULL,	      NULL,		  0,		    1,	         -1 },
 };
 
