bb237818513728f3bb40c0f3bb064bb2e699519a
basi-a <basi-a@outlook.com>
2023-01-08 09:53:30 +0800

    update dotfile

--- use-dotfiles.sh
+++ use-dotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ln-sf(){
     path="`pwd`"
-    ln -sf $path/dotfiles/.zshrc $HOME/.zshrc
-    ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
     ln -sf $path/dotfiles/config/* $HOME/.config
     ln -sf $path/dotfiles/dwm $HOME/.dwm
-    ln -sf $path/dotfiles/xinitrc/.xinitrc $HOME/.xinitrc
+    ln -sf $path/dotfiles/.zshrc $HOME/.zshrc
+    ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
+    ln -sf $path/dotfiles/.xinitrc $HOME/.xinitrc
 }
 
 main(){
