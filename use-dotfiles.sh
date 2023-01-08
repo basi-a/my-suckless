edd32d7ebbdb30acf8f21f57b4e29adcf77ee1ca
basi-a <basi-a@outlook.com>
2023-01-08 09:13:54 +0800

    add my zshrc

--- use-dotfiles.sh
+++ use-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ln-sf(){
     path="`pwd`"
+    ln -sf $path/dotfiles/.zshrc $HOME/.zshrc
     ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
     ln -sf $path/dotfiles/config/* $HOME/.config
     ln -sf $path/dotfiles/dwm $HOME/.dwm
