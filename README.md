cada7661fe080313171094382b579d94775e6b13
basi-a <basi-a@outlook.com>
2023-01-10 20:16:33 +0800

    patch scratchpad

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ alt+F2          减小音量
 alt+F3          增大音量
 alt+F4          降低亮度
 alt+F5          提升亮度
-
+alt+[xk_grave]       打开位于屏幕正中的小终端默认st， 按第二遍隐藏 ; xk_grave 是tab上面，esc下面的倒引号那个键
 #标签、窗口类
 alt+shift+j/k   将栈中窗口挪到主窗口
 alt+left/right  切换前一个或下一个tag
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -78,6 +78,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, topbar ? NULL : "-b", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 //static const char *browsercmd[]  = { "chromium", NULL };
 static const char *browsercmd[]  = { "edge", NULL };
@@ -103,6 +105,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_F4,     spawn,          {.v = lightdown } },
 	{ MODKEY,                       XK_F5,     spawn,          {.v = lightup } },
 	{ MODKEY,                       XK_r,      spawn,          {.v = roficmd } },
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
