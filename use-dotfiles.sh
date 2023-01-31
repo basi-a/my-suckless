4af8a5a463bc4e9c41ed47696cda41ebd5792543
basi-a <basi-a@outlook.com>
2023-01-31 09:21:31 +0800

    update script and dotfile

--- use-dotfiles.sh
+++ use-dotfiles.sh
@@ -2,7 +2,7 @@
 ln-sf(){
     path="`pwd`"
     ln -sf $path/dotfiles/config/* $HOME/.config
-    ln -sf $path/dotfiles/dwm $HOME/.dwm
+    ln -sf $path/dotfiles/.dwm $HOME
     ln -sf $path/dotfiles/.zshrc $HOME/.zshrc
     ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
     ln -sf $path/dotfiles/.xinitrc $HOME/.xinitrc
