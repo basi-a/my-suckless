9e4facb77b77a15180ac0e3590cd927c3f8cb932
basi-a <basi-a@outlook.com>
2023-02-10 17:42:23 +0800

    add zsh theme

--- use-dotfiles.sh
+++ use-dotfiles.sh
@@ -3,9 +3,10 @@ ln-sf(){
     path="`pwd`"
     ln -sf $path/dotfiles/config/* $HOME/.config
     ln -sf $path/dotfiles/.dwm $HOME
-    ln -sf $path/dotfiles/.zshrc $HOME/.zshrc
+    ln -sf $path/dotfiles/zsh/.zshrc $HOME/.zshrc
     ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
     ln -sf $path/dotfiles/.xinitrc $HOME/.xinitrc
+    sudo ln -sf $path/dotfiles/zsh/dracula.zsh-theme /usr/share/oh-my-zsh/themes/dracula.zsh-theme
 }
 
 main(){
