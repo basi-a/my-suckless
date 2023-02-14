6a992a2f92509b2e632e8c482a706a4f55870e4a
basi-a <basi-a@outlook.com>
2023-02-14 11:37:04 +0800

    update script

--- use-dotfiles.sh
+++ use-dotfiles.sh
@@ -3,14 +3,25 @@ ln-sf(){
     path="`pwd`"
     ln -sf $path/dotfiles/config/* $HOME/.config
     ln -sf $path/dotfiles/.dwm $HOME
-    ln -sf $path/dotfiles/.cargo $HOME
+    ln -sf $path/dotfiles/.cargo/config $HOME/.cargo/config
     ln -sf $path/dotfiles/zsh/.zshrc $HOME/.zshrc
     ln -sf $path/dotfiles/.gtkrc-2.0 $HOME/.gtkrc-2.0
     ln -sf $path/dotfiles/.xinitrc $HOME/.xinitrc
     sudo ln -sf $path/dotfiles/zsh/dracula.zsh-theme /usr/share/oh-my-zsh/themes/dracula.zsh-theme
 }
-
+makedir(){
+    if [ ! -d "$HOME/.config" ];then
+        mkdir $HOME/.config
+    fi
+    if [ ! -d "$HOME/go/bin" ];then
+        mkdir -p $HOME/go/bin
+    fi
+    if [ ! -d "$HOME/.cargo" ];then
+        mkdir $HOME/.cargo
+    fi
+}
 main(){
+    makedir
     ln-sf
 }
 main
