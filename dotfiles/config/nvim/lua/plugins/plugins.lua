faecb3f47593176439693f03aa3e6f7115f41b93
basi-a <basi-a@outlook.com>
2023-05-28 19:35:19 +0800

    del nvim

--- dotfiles/config/nvim/lua/plugins/plugins.lua
+++ dotfiles/config/nvim/lua/plugins/plugins.lua
@@ -1,4 +1,4 @@
-local ensure_packer = function()
+Clocal ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
