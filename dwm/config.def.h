eb61db6e151abe48bb19757c82794c5d6592b5c0
basi-a <basi-a@outlook.com>
2024-01-04 22:35:56 +0800

    add thunderbird rule

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -52,6 +52,7 @@ static const Rule rules[] = {
 	{ "wemeet",   NULL,       NULL,       0,            1,           -1 },
 	{ "virtualbox",   NULL,       NULL,       0,            1,           -1 },
 	{ "minecraft-launcher",   NULL,       NULL,       0,            1,           -1 },
+	{ "thunderbird",   NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
