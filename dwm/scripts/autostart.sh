9eee9445ffcfb81bfb840814dc48e64289479a7a
basi-a <basi-a@outlook.com>
2024-02-07 12:36:58 +0800

    add config and fix scripts

--- dwm/scripts/autostart.sh
+++ dwm/scripts/autostart.sh
@@ -7,4 +7,11 @@ done &
 numlockx &
 fcitx5 -d &
 slstatus &
-xautolock -time 15 -locker slock &
+xautolock -time 60 -locker slock &
+wmname LG3D &
+/usr/lib/pam_kwallet_init &
+sleep 2 && \
+/usr/lib/polkit-kde-authentication-agent-1 &
+birdtray &
+dunst &
+source ups.sh
