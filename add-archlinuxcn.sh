a90ae0b366034dbaa023883bf8f636f829051f11
basi-a <basi-a@outlook.com>
2024-02-24 20:45:28 +0800

    change config and fix script

--- add-archlinuxcn.sh
+++ add-archlinuxcn.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cat > /etc/pacman.conf << EOF
+cat >> /etc/pacman.conf << EOF
 [archlinuxcn]
 Include = /etc/pacman.d/archlinuxcn-mirrorlist
 EOF
-cat > /etc/pacman.d/archlinuxcn-mirrorlist <<EOF
-Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
-EOF
+echo 'Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' | tee -a /etc/pacman.d/archlinuxcn-mirrorlist
 pacman -Sy archlinuxcn-keyring
